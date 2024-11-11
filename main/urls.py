289c3919baef6a2439b52dfe12f70b84f3e11041
nistrib <158342833+nistrib@users.noreply.github.com>
2024-11-11 18:59:55 -0500

    Version with log in page

--- main/urls.py
+++ main/urls.py
@@ -1,14 +1,22 @@
 from django.urls import path
 from . import views
 
+
+# If want to use without log in page at start
+# urlpatterns = [
+#     path('login/', views.login_view, name='login'),
+#     path('', views.home, name='home'),
+#     path('project_control_center/', views.project_control_center, name='project_control_center'),
+#
+#     path('about/', views.about, name='about'),
+# ]
+
+
+
+# If want to use with a log in page
 urlpatterns = [
-    path('login/', views.login_view, name='login'),
-
-    path('', views.home, name='home'),
-
+    path('', views.login_view, name='login'),  # Set the login view as the default route
+    path('home/', views.home, name='home'),  # Make home accessible after login
     path('project_control_center/', views.project_control_center, name='project_control_center'),
-
     path('about/', views.about, name='about'),
 ]
-
-# path('projects/<int:pk>/', views.project_detail, name='project_detail'),
--- main/views.py
+++ main/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
 
 from main.models import Project
 
